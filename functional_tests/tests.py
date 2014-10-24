cddf5650efe5d534b7f4b31aa54873826ad0057c
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-10-23 20:05:14 -0700

    Use bootstrap to make things look a bit better

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,11 @@
 """functional tests for our to-do app"""
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """unittest class"""
     def setUp(self):
         self.browser = webdriver.Firefox()
