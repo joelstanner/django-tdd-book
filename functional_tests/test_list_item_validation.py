e3cea3a206afba39ad24780094d40aab4b750232
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-10-29 20:26:55 -0700

    End of chapter 12 - added duplicate items checking and tests.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -46,4 +46,4 @@ class ItemValidationTest(FunctionalTest):
         # She sees a helpful and stylish error message
         self.check_for_row_in_list_table('1: Buy wellies')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You've already got this in your list")
+        self.assertEqual(error.text, "Duplicate items not added")
