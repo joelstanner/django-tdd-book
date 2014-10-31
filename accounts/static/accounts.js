110f8db728157e44411e76ff413bb6cd14b80bc8
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-10-31 04:21:13 -0700

    stopped at "Checking Call Arguments" ch15l042

---
+++ accounts/static/accounts.js
@@ -0,0 +1,15 @@
+/*global $ */
+
+var initialize = function (navigator) {
+    $('#id_login').on('click', function () {
+        navigator.id.request();
+    });
+
+    navigator.id.watch();
+};
+
+window.Superlists = {
+    Accounts: {
+        initialize: initialize
+    }
+};
