bda6978e71e90eb8dfe6b720b9e00834487c223e
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-11-01 19:40:58 -0700

    fix #3

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -2,7 +2,8 @@ from os import path
 import subprocess
 THIS_FOLDER = path.dirname(path.abspath(__file__))
 
-# try changing change fab2 to fab if it's not working.
+# try changing change fab2 to fab if it's not working. - maybe remove the
+# "poolbath1@" and ":223" as well
 def create_session_on_server(host, email):
     return subprocess.check_output(
         ['fab2',
