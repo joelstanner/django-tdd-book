82c2188041b948fcb8aee3508cc147897611c5b6
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-11-09 17:49:11 -0800

    use wait_for function for URL checks in my_lists

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,16 +1,20 @@
 """base functional tests for our to-do app - helper class"""
 
+import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import WebDriverException
 import sys
-from .server_tools import reset_database
 import os
+from .server_tools import reset_database
 from datetime import datetime
 
+DEFAULT_WAIT = 5
 SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
 )
+
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -37,7 +41,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         if self.against_staging:
             reset_database(self.server_host)
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(DEFAULT_WAIT)
 
     def tearDown(self):
         if self._test_has_failed():
@@ -50,7 +54,6 @@ class FunctionalTest(StaticLiveServerTestCase):
                 self.dump_html()
         self.browser.quit()
         super().tearDown()
-
 
     def _test_has_failed(self):
         for method, error in self._outcome.errors:
@@ -79,6 +82,16 @@ class FunctionalTest(StaticLiveServerTestCase):
             windowid=self._windowid,
             timestamp=timestamp
         )
+
+    def wait_for(self, function_with_assertion, timeout=DEFAULT_WAIT):
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            try:
+                return function_with_assertion()
+            except (AssertionError, WebDriverException):
+                time.sleep(0.1)
+        # one more try, which will raise any errors if they are outstanding
+        return function_with_assertion()
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -16,7 +16,6 @@ from lists.views import new_list
 
 class HomePageTest(TestCase):
 
-
     def test_home_page_renders_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
